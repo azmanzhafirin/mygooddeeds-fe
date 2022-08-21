ca7236b391005e6808dba8645195974e20b6ebb3
Dharma <dharmaseelan970@gmail.com>
2022-08-21 21:24:19 +0800

    first commit

---
+++ src/components/main_banner/Main_Banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Main_Banner() {
+  return (
+    <div className="main_banner">
+      <div className="container-fluid">
+        <h1>Main Banner</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Main_Banner;
