3e235a77528e40b786cd62da24f2ebde84ebd459
Azman Zhafirin <azman.rosman@uberfusion.com>
2022-08-30 23:14:25 +0800

    Split feed into smaller component with dummy data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/header/Header";
 import Main_Banner from "./components/main_banner/Main_Banner";
 import Count from "./components/count/Count";
-import Feed from "./components/feed/Feed";
+import FeedList from "./components/feed/FeedList";
 import Cta_Banner from "./components/cta_banner/Cta_Banner";
 import Latest_Blogs from "./components/latest_blogs/Latest_Blogs";
 import Cta_Content from "./components/cta_content/Cta_Content";
@@ -13,7 +13,7 @@ function App() {
       <Header />
       <Main_Banner />
       <Count />
-      <Feed />
+      <FeedList />
       <Cta_Banner />
       <Latest_Blogs />
       <Cta_Content />
--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,155 +1,28 @@
 import React from "react";
-import { Navigation, Pagination } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
 import Feedbanner from "../../assets/images/feed-placeholder.jpeg";
 
-//Import styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-import "./Feed.scss";
 
-function Feed() {
-  return (
-    <div className="Feed">
-      <div className="container">
-        <div className="title-wrapper">
-          <div className="title-h2">
-            <h2>Ongoing Campaigns</h2>
-          </div>
-          <p className="title-content">
-            You can help lots of people by donating little.
-          </p>
-        </div>
+function Feed(props){
+    return(
+    <li className="list">
+    <div className="feed-banner">
+      <img src={Feedbanner} alt="feedbanner" />
+      <div className="t-donations">55</div>
+    </div>
+    <div className="feed-details">
+      <h6>{props.title}</h6>
+      <p className="desc">
+        {props.description}
+      </p>
 
-        <ul className="feed-wrapper">
-          <Swiper
-            modules={[Navigation, Pagination]}
-            //navigation
-            pagination={{ clickable: true }}
-            spaceBetween={30}
-            slidesPerView={4}
-          >
-            <SwiperSlide>
-              <li className="list">
-                <div className="feed-banner">
-                  <img src={Feedbanner} alt="feedbanner" />
-                  <div className="t-donations">55</div>
-                </div>
-                <div className="feed-details">
-                  <h6>Support the Bulla Family</h6>
-                  <p className="desc">
-                    Jake Bulla was involved in a severe motorcycle accident
-                    recently and as a result, was flown to Royal Perth Hospital
-                    and was admitted to the Intensive Care Unit.
-                  </p>
-
-                  <div className="donations-details">
-                    <span className="t-raised">Rm 5,570 raised</span>
-                  </div>
-                  <div className="donate-wrapper">
-                    <div className="donate-btn">Donate</div>
-                  </div>
-                </div>
-              </li>
-            </SwiperSlide>
-            <SwiperSlide>
-              <li className="list">
-                <div className="feed-banner">
-                  <img src={Feedbanner} alt="feedbanner" />
-                  <div className="t-donations">12</div>
-                </div>
-                <div className="feed-details">
-                  <h6>Dale Olsson Memorial Fund</h6>
-                  <p className="desc">
-                    Jake Bulla was involved in a severe motorcycle accident
-                    recently and as a result, was flown to Royal Perth Hospital
-                    and was admitted to the Intensive Care Unit.
-                  </p>
-
-                  <div className="donations-details">
-                    <span className="t-raised">Rm 1,570 raised</span>
-                  </div>
-                  <div className="donate-wrapper">
-                    <div className="donate-btn">Donate</div>
-                  </div>
-                </div>
-              </li>
-            </SwiperSlide>
-            <SwiperSlide>
-              <li className="list">
-                <div className="feed-banner">
-                  <img src={Feedbanner} alt="feedbanner" />
-                  <div className="t-donations">4</div>
-                </div>
-                <div className="feed-details">
-                  <h6>Nala Needs Surgery</h6>
-                  <p className="desc">
-                    Jake Bulla was involved in a severe motorcycle accident
-                    recently and as a result, was flown to Royal Perth Hospital
-                    and was admitted to the Intensive Care Unit.
-                  </p>
-
-                  <div className="donations-details">
-                    <span className="t-raised">Rm 4,570 raised</span>
-                  </div>
-                  <div className="donate-wrapper">
-                    <div className="donate-btn">Donate</div>
-                  </div>
-                </div>
-              </li>
-            </SwiperSlide>
-            <SwiperSlide>
-              <li className="list">
-                <div className="feed-banner">
-                  <img src={Feedbanner} alt="feedbanner" />
-                  <div className="t-donations">17</div>
-                </div>
-                <div className="feed-details">
-                  <h6>Raise money for Tails Chemotherapy Chemotherapy</h6>
-                  <p className="desc">
-                    Jake Bulla was involved in a severe motorcycle accident
-                    recently and as a result, was flown to Royal Perth Hospital
-                    and was admitted to the Intensive Care Unit.
-                  </p>
-
-                  <div className="donations-details">
-                    <span className="t-raised">Rm 9,570 raised</span>
-                  </div>
-                  <div className="donate-wrapper">
-                    <div className="donate-btn">Donate</div>
-                  </div>
-                </div>
-              </li>
-            </SwiperSlide>
-            <SwiperSlide>
-              <li className="list">
-                <div className="feed-banner">
-                  <img src={Feedbanner} alt="feedbanner" />
-                  <div className="t-donations">35</div>
-                </div>
-                <div className="feed-details">
-                  <h6>Support the Bulla Family</h6>
-                  <p className="desc">
-                    Jake Bulla was involved in a severe motorcycle accident
-                    recently and as a result, was flown to Royal Perth Hospital
-                    and was admitted to the Intensive Care Unit.
-                  </p>
-
-                  <div className="donations-details">
-                    <span className="t-raised">Rm 8,5570 raised</span>
-                  </div>
-                  <div className="donate-wrapper">
-                    <div className="donate-btn">Donate</div>
-                  </div>
-                </div>
-              </li>
-            </SwiperSlide>
-          </Swiper>
-        </ul>
+      <div className="donations-details">
+        <span className="t-raised">{props.totalDonation}</span>
+      </div>
+      <div className="donate-wrapper">
+        <div className="donate-btn">Donate</div>
       </div>
     </div>
+  </li>
   );
 }
 
